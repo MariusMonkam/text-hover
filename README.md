2e0d7e635bc6d9d411bfbb15a5a34644426e85dd
MariusMonkam <cloudmonde@yahoo.com>
2019-09-27 22:47:00 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-zfbrec
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-zfbrec)
---
+++ index.js
@@ -0,0 +1,10 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+
+
+
+      document.getElementById('text').innerHTML=
+"Amazing World!"      
+       
